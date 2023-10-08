b6f9fd81b2f89f2c51d4ba61c0e8675e01e725e7
Niklas Hanft (mointech) <nh@mointech.de>
2023-10-08 11:01:44 +0200

    Add test helper

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -7,16 +7,20 @@ func TestHello(t *testing.T) {
 		got := Hello("Niklas")
 		want := "Hello, Niklas"
 
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 	t.Run("", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
 
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
