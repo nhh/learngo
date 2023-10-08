abc17d9cc37b8cd401d49c57defbfa667dee04e5
Niklas Hanft (mointech) <nh@mointech.de>
2023-10-08 11:23:28 +0200

    Add spanish and binary as languages

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -3,15 +3,32 @@ package main
 import "fmt"
 
 const englishPrefix = "Hello, "
+const spanishPrefix = "Hola, "
 
-func Hello(name string) string {
+func convertStringToBinary(str string) string {
+	binaryString := ""
+	for _, char := range str {
+		binaryString = fmt.Sprintf("%s%.8b", binaryString, char)
+	}
+	return binaryString
+}
+
+func Hello(name string, lang string) string {
 	if name == "" {
 		name = "World"
 	}
 
-	return englishPrefix + name
+	switch lang {
+	case "English":
+		return englishPrefix + name
+	case "Spanish":
+		return spanishPrefix + name
+	default:
+		return convertStringToBinary(englishPrefix) + convertStringToBinary(name)
+	}
+
 }
 
 func main() {
-	fmt.Println(Hello("Niklas"))
+	fmt.Println(Hello("Niklas", "English"))
 }
--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -4,15 +4,25 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Niklas")
+		got := Hello("Niklas", "English")
 		want := "Hello, Niklas"
 
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("empty string defaults to 'world'", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "English")
 		want := "Hello, World"
 
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in spanish!", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in binary!", func(t *testing.T) {
+		got := Hello("", "")
+		want := "010010000110010101101100011011000110111100101100001000000101011101101111011100100110110001100100"
 		assertCorrectMessage(t, got, want)
 	})
 }
