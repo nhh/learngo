28ba68079b2defef06711629f4e22a0d490800fc
Niklas Hanft (mointech) <nh@mointech.de>
2023-10-08 15:21:00 +0200

    Add iteration

---
+++ iteration/iteration.go
@@ -0,0 +1,11 @@
+package iteration
+
+const repeatCount = 5
+
+func Repeat(str string) string {
+	var repeated string
+	for i := 0; i < repeatCount; i++ {
+		repeated += str
+	}
+	return repeated
+}
