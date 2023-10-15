85d70f83cf7dbbc6082a77da6575f58f2b33327b
Niklas Hanft (mointech) <nh@mointech.de>
2023-10-15 11:12:32 +0200

    Add simple concurrency example

---
+++ concurrency/concurrency.go
@@ -0,0 +1,28 @@
+package concurrency
+
+type WebsiteChecker func(string) bool
+
+type result struct {
+	string
+	bool
+}
+
+func captureResult(u string, wc WebsiteChecker, channel chan<- result) {
+	channel <- result{u, wc(u)}
+}
+
+func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
+	results := make(map[string]bool)
+	resultChannel := make(chan result)
+
+	for _, url := range urls {
+		go captureResult(url, wc, resultChannel)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		r := <-resultChannel
+		results[r.string] = r.bool
+	}
+
+	return results
+}
